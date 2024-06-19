d7cdce203894819940ce6589f174fe0d072a2df3
Fernandohka <fernando2005andrade@gmail.com>
2024-06-19 16:44:08 -0300

    commit

--- blocks.h
+++ blocks.h
@@ -3,26 +3,36 @@
 #include <stdio.h>
 #include "imports.h"
 
-FILE * archive;
-
-LList *readArchive()
-{
-    LList *list;
-    LListCreate(list);
-    char line[10];
-    
-    archive = fopen("Map.txt", "r");
-
-    LListAdd(list, (line, 10, archive));
-
-    return list;
-}
-
 typedef struct block
 {
     int x;
     int y;
     int type;
 } block;
+
+FILE * archive;
+
+LList *readArchive()
+{
+    int n1, n2, n3;
+    int count = 0;
+    FILE * File = fopen("Map.txt", "r");
+    LList *lista;
+    LListCreate(lista);
+
+    block *bloco;
+
+    while(!feof(File))
+    {
+        ++count;
+        fscanf(File, "%i,%i,%i", &bloco->x, &bloco->y, &bloco->type);
+        LListAdd(lista, bloco);
+    }
+    for(int i = 0; i < 100; i++)
+    {
+        printf(LListGet(lista, i));
+    }
+}
+
 
 #endif 
