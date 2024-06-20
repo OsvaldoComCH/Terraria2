cd5f53488c55a8f6d49eb64a6ee370fe07bb0a78
OsvaldoComCH <eduhrsilva@gmail.com>
2024-06-20 16:02:47 -0300

    Syncing

--- input.c
+++ input.c
@@ -3,15 +3,31 @@
 void MoveLeft(character * Player, int Pixels)
 {
     Player->xSubPos -= Pixels;
-    if(Player->xSubPos < -16)
+    if(Player->xSubPos < 0)
     {
-        Player->xPos += (Player->xSubPos - 16)/32;
+        Player->xPos -= Player->xSubPos / 32 + 1;
+        Player->xSubPos = 32 - Player->xSubPos % 32;
+    }
+}
+
+void MoveRight(character * Player, int Pixels)
+{
+    Player->xSubPos += Pixels;
+    if(Player->xSubPos > 31)
+    {
+        Player->xPos += Player->xSubPos / 32;
+        Player->xSubPos = Player->xSubPos % 32;
     }
 }
 
 void input(character * Player)
 {
-    if(GetAsyncKeyState(VK_W))
+    if(GetAsyncKeyState(VK_A))
     {
+        MoveLeft(Player, 3);
+    }
+    if(GetAsyncKeyState(VK_D))
+    {
+        MoveRight(Player, 3);
     }
 }
--- render.c
+++ render.c
@@ -1,15 +1,21 @@
 #include "imports.h"
 #include "functions.c"
 
-void DrawMap(const LList * Map, HWND hwnd)
+void RenderMap(const LList * Map, HWND hwnd)
 {
     HDC hdc = GetDC(hwnd);
     LLNode * N = Map->Head;
     for(int i = 0; i < Map->Size; ++i)
     {
         block * B = (block *) N->Value;
-        DrawImg(hdc, B->x*32, 649 - (B->y*32), 32, 32, B->img);
+        DrawImg(hdc, B->x*32, 649 - (B->y*32), 31, 31, B->img);
         N = N->Next;
     }
     ReleaseDC(hwnd, hdc);
+}
+
+void RenderPlayer(const character * Player, HWND hwnd)
+{
+    HDC hdc = GetDC(hwnd)
+    
 }
